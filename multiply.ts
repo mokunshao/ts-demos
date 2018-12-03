ac2132514707578453c57c61ea36846258514921
mokunshao <1627510957@qq.com>
2018-12-03 16:50:59 +0800

    Create multiply.ts

---
+++ multiply.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env ts-node
+export const a: number = parseInt(process.argv[2]);
+export const b: number = parseInt(process.argv[3]);
+
+if (Number.isNaN(a) || Number.isNaN(b)) {
+  console.log("输入不合法");
+  process.exit(1);
+}
+
+console.log(a * b);
+process.exit(0);
